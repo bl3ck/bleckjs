7050d3acafc5a84c5769ce03261a090c874915bd
bl3ck <eugenbleck@gmai.com>
2017-05-28 09:32:15 +0100

    Create a folder ch2 to hold solution for chapter 2 exercise -- Solved looping challenge exercise

---
+++ chp2/looping-triangle.js
@@ -0,0 +1,11 @@
+/*
+* Looping Triangles
+* --- Eugen-Bleck
+*/
+for (var i=0; i < 7; i++){
+    for(var j = 0; j < 7; j++){
+        if(i === j){
+            console.log('*'.repeat(i+1));
+        }
+    }
+}
