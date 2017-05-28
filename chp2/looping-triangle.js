31c00570ce5f8251dc294391aa14395f1117d33f
bl3ck <eugenbleck@gmai.com>
2017-05-28 11:41:23 +0100

    Solution as required by textbook

--- chp2/looping-triangle.js
+++ chp2/looping-triangle.js
@@ -2,10 +2,6 @@
 * Looping Triangles
 * --- Eugen-Bleck
 */
-for (var i = 0;  i < 7; i++){
-    for(var j = 0; j < 7; j++){
-        if(i === j){
-            console.log('*'.repeat(i+1));
-        }
-    }
+for (var line = '#';  line.length < 8; line += "#"){
+    console.log(line);
 }
